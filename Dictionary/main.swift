32c2e44a79c957416acf00f35e52ee012cba2244
Vampirius1 <111462054+Vampirius1@users.noreply.github.com>
2022-09-29 18:45:50 +0300

    initial Dictionary

--- Dictionary/main.swift
+++ Dictionary/main.swift
@@ -7,5 +7,23 @@
 
 import Foundation
 
-print("Hello, World!")
+//MARK: -- Доступ к элементам словаря
+var numbersDictionary = [0:"Zero", 1:"One", 2:"Two"]
+print("The numbers dictionary contains \(numbersDictionary.count) items.")
 
+print("isProperty equals \(numbersDictionary.isEmpty)")
+
+numbersDictionary[5] = "Five"
+
+if let eleven = numbersDictionary[11] {
+    print("The name of number is \(eleven).")
+} else {
+    print("The name of number isn't exist.")
+}
+
+//это выражение возвращает удаленный элемент
+let removeValue = numbersDictionary.removeValue(forKey: 0)
+
+for (key, value) in numbersDictionary {
+    print("\(key): \(value)")
+}
